58e32e929d32c6041f9c3462a7d2df8ba3d51b53
jepst <617498+jepst@users.noreply.github.com>
2022-12-30 14:50:57 -0500

    FIX call right bash
    The test environment should call jbash, not regular bash

--- theia/autograde/anubis_autograde/shell/run.py
+++ theia/autograde/anubis_autograde/shell/run.py
@@ -8,4 +8,4 @@ def run_debug_shell(_: argparse.Namespace):
     if not os.path.exists('.bashrc'):
         log.error(f'.bashrc does not exist. Please run in directory with autograde server running.')
         exit(1)
-    os.system('bash --rcfile .bashrc')
+    os.system('jbash --rcfile .bashrc')
